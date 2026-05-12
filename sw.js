28d1593875571f30261329568bbe1f8b34a0d47c
mdnayonsarker1-art <mdnayonsarker1@gmail.com>
2026-05-13 01:39:36 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,37 @@
-const cacheName = 'sharif-pharma-v2';
+const cacheName = 'sharif-pharma-v3';
 const assets = [
   './',
   './index.html',
   './settings.html',
   './store.html',
   './history.html',
+  './order.html',
   './style.css',
   './config.js',
+  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// ইনস্টল করার সময় ফাইলগুলো ক্যাশে জমা করা
+// ফাইলগুলো ফোনে সেভ করা
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-// অফলাইনে কাজ করার জন্য ফেচিং লজিক
+// অফলাইনে ফাইলগুলো পরিবেশন করা
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      // যদি ক্যাশে ফাইল থাকে তবে ক্যাশ থেকেই দিবে, নাহলে ইন্টারনেট থেকে লোড করবে
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
 
-// পুরনো ক্যাশ ডিলিট করা
+// পুরনো ফাইল মুছে ফেলা
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+    ))
   );
 });
